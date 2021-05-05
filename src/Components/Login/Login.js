e9e47feb239e67d2445efaf62b18d4595281611c
Marshall Johnson <marshalljohnson193@gmail.com>
2021-05-05 14:17:12 -0600

    boiler plate

---
+++ src/Components/Login/Login.js
@@ -0,0 +1,7 @@
+import react, { userState, useEffect } from 'react'
+
+function Login() {
+    return <div>Login.js</div>
+}
+
+export default Login
---
+++ src/Components/Tasks/Tasks.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Tasks(props) {
+    return (
+        <div>
+            Tasks.js
+        </div>
+    );
+}
+
+export default Tasks;
